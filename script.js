e6154a9b80772ab9226437487a4d9dc09063b165
sarsac <sachinsaravanant@gmail.com>
2020-10-21 21:05:24 +0400

    dump

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let INTERVAL = 1;
+
 const page = {
     question: $("#question"),
     clues: $("#clues"),
@@ -8,19 +10,36 @@ const page = {
     },
     answer: $("#answer-box")
 };
+const timer = {
+    ans: {
+        track: $("#timer-ans-track"),
+        bar: $("#timer-ans-bar"),
+        num: $("#timer-ans-num")
+    },
+    clue: {
+        track: $("#timer-clue-track"),
+        bar: $("#timer-clue-bar"),
+        num: $("#timer-clue-num")
+    }
+};
+
 
 class Stage {
     constructor(begin_time) {
         this.begin_time = begin_time;
-        this.interval = 0;
+        this.entry_time = this.begin_time;
 
+        this.set_time();
         this.set_answer();
         this.set_question();
 
         this.clues = [];
         this.clue_count = 0;
     }
-    set_answer(data = "answer", exp = "") {
+    set_time(time = 0) {
+        this.time = time;
+    }
+    set_answer(data = null, exp = "") {
         this.answer = data;
         this.answer_exp = "";
         return this;
@@ -30,27 +49,33 @@ class Stage {
         return this;
     }
     add_clue(clue, time) {
-        this.interval += time;
-        this.clues.push([clue, this.begin_time + this.interval]);
+        if(this.clues.length > 0){
+            let [_, prev, __] = this.clues[this.clues.length - 1];
+            this.clues.push([clue, time, prev]);
+        }
+        else {
+            this.clues.push([clue, time, 0]);
+        }
+
         return this;
     }
-    push_end(time) {
-        this.interval += time;
-    }
-    get_end(){
-        return this.begin_time + this.interval;
+    get_end() {
+        return this.begin_time + this.time;
     }
 
     match_answer(answer) {
         const process = name => name.toLowerCase().split().join();
-        return process(this.answer) === process(answer);    
+        return process(this.answer) === process(answer);
     }
 
+    start(){
+        this.entry_time = Date.now(); 
+    }
     update_clue() {
         if (this.clue_count < this.clues.length) {
-            const [clue, time] = this.clues[this.clue_count];
+            const [clue, time, _] = this.clues[this.clue_count];
 
-            if (time < Date.now()) {
+            if (time < Date.now() - this.entry_time) {
                 const node = document.createElement("div");
                 node.classList.add("card", "clue");
 
@@ -76,46 +101,56 @@ class Stage {
         const node = document.createElement("div");
         node.classList.add("card", "clue");
         node.innerHTML = `The answer is ${this.answer}`;
-        
-        if(this.answer_exp.length > 0) {
+
+        if (this.answer_exp.length > 0) {
             node.innerHTML += `<br>(${this.answer_exp})`;
         }
-        
+
         page.clues.appendChild(node);
         this.display_answer = () => { };
     }
     update() {
-        let P = (Date.now() - this.begin_time) / this.interval;
-        if (0 <= P && P <= 1) {
-            page.timer.bar.style.width = `${100 * P}%`;
+        const T = Date.now();
+        
+        if(this.clue_count < this.clues.length) {
+            const [_, time, prev] = this.clues[this.clue_count];
 
-            if (this.clue_count < this.clues.length) {
-                const [_, time] = this.clues[this.clue_count];
-                const t = format_time(time - Date.now());
-                const c = `${this.clue_count + 1} of ${this.clues.length}`
-                page.timer.num.innerHTML = `Clue ${c}, in ${t}`;
+            const t_c = format_time(time - (T - this.entry_time));
+
+            const c = `${this.clue_count + 1} of ${this.clues.length}`
+            timer.clue.num.innerHTML = `Clue ${c}, in ${t_c}`;
+            
+            timer.clue.track.style.display = "inline-block";
+        
+            let P = (T - this.entry_time - prev)/(time - prev);
+            timer.clue.bar.style.width = `${100 * P}%`;
+        }
+        else {
+            timer.clue.num.innerHTML = "";
+            timer.clue.track.style.display = "none";
+        }
+
+
+        let P = (T - this.begin_time) / this.time;
+        if (P <= 1) {
+            const t_a = format_time(this.get_end() - T);
+            timer.ans.num.innerHTML = `Answer in ${t_a}`;
+
+            if(P < 0){
+                timer.ans.track.style.display = "none";
+                timer.ans.bar.style.width = "100%";        
             }
             else {
-                const t = format_time(this.begin_time + this.interval - Date.now());
-                page.timer.num.innerHTML = `${t} remaining`
+                timer.ans.track.style.display = "inline-block";
+                timer.ans.bar.style.width = `${100 * clamp(P, 0, 1)}%`;        
             }
         }
-        else if (P < 0) {
-            const t = format_time(this.begin_time - Date.now());
-            page.timer.num.innerHTML = `Stage starting in ${t}`
-            page.timer.bar.style.width = `100%`;
-        }
         else if (P > 1) {
+            timer.ans.track.display = "none";
             this.display_answer();
         }
 
         this.update_clue();
-    }
-    displace_time(time) {
-        //this.begin_time += time;
-        for(let i = 0; i < this.clues.length; i++){
-            this.clues[i][1] += time;
-        }
     }
 }
 
@@ -124,23 +159,23 @@ class StageManager {
         this.begin_time = begin_time;
         this.stages = [];
         this.current_stage = 0;
-        this.set_synchronized();
     }
 
-    set_synchronized(sync = true){
-        this.synchronized = sync;
-    }
     add_stage() {
         let begin_time = this.begin_time;
-
-        let prev = this.stages[this.stages.length - 1];
-        if (prev !== undefined) {
-            begin_time += prev.interval;
+        if(this.stages.length > 0) {
+            begin_time = this.stages[this.stages.length - 1].get_end()
         }
 
-        stage = new Stage(begin_time);
-        this.stages.push(stage)
+        let stage = new Stage(begin_time);
+        this.stages.push(stage);
         return stage;
+    }
+    get_end() {
+        if(this.stages.length > 0){
+            return this.stages[this.stages.length - 1].get_end();
+        }
+        return this.begin_time;
     }
 
     start_game(progress_bar) {
@@ -153,9 +188,6 @@ class StageManager {
             if (i === 0) {
                 node.classList.add("current");
             }
-
-            node.innerHTML = i + 1;
-
             page.progress_bar.appendChild(node);
         }
 
@@ -164,6 +196,7 @@ class StageManager {
     setup_stage() {
         let stage = this.stages[this.current_stage];
         page.question.innerHTML = stage.question;
+        stage.start();
     }
 
     next_stage() {
@@ -187,35 +220,13 @@ class StageManager {
             end();
         }
     }
-    displace_time(time) {
-        this.begin_time += time;
-        for(let child of this.stages){
-            child.displace_time(time);
-        }
-    }
-    submit_answer(answer){
+    submit_answer(answer) {
         let stage = this.stages[this.current_stage];
-        let t = Date.now();
-
-        const update = () => {
-            if(!this.synchronized && this.current_stage < this.stages.length) {
-                stage = this.stages[this.current_stage];
-                let delta = stage.begin_time - t;
-                this.displace_time(-delta);    
-                this.update();
-            }
+        if (Date.now() > stage.get_end() || stage.match_answer(answer)) {
+            this.next_stage();
+            return true;
         }
-
-        if(stage.begin_time < t) {
-            if(t > stage.get_end()) {
-                this.next_stage();
-                update();
-            }
-            else if(stage.match_answer(answer)) {                
-                this.next_stage();
-                update();
-            }    
-        }
+        return false;
     }
 
     update() {
@@ -243,8 +254,8 @@ const format_time = milli => {
 
 let sman;
 const setup = time => {
-    let C = convert_time(0, 30)
-    let E = convert_time(0, 30);
+    let C = convert_time(0, 10);
+    let I = convert_time(0, 20);
 
     //Im not getting paid so i dont have time to encrypt it properly, 
     //this will do to deter the average person
@@ -262,10 +273,6 @@ const setup = time => {
     ]
 
     sman = new StageManager(time);
-    sman.add_stage()
-        .set_question("Begin Quiz")
-        .set_answer("")
-        .push_end(0);
 
     sman.add_stage()
         .set_question(
@@ -278,10 +285,10 @@ const setup = time => {
         .add_clue(
             `The movie’s main protagonist was also in the 
             same set of movies as James McAvoy`
-        , C)
+            , C)
         .set_answer(answers.shift(), "Assassin’s Creed")
-        .push_end(E);
-    
+        .set_time(I);
+
     sman.add_stage()
         .set_question(
             `There is a book about an unfortunately named
@@ -291,11 +298,11 @@ const setup = time => {
         )
         .add_clue(
             `The film adaptation of the novel stars Kevin
-            from How I Met Your Mother` 
-        , C)
+            from How I Met Your Mother`
+            , C)
         .set_answer(answers.shift(), "The Namesake – First Book")
-        .push_end(E);
-    
+        .set_time(I);
+
     sman.add_stage()
         .set_question(
             `I was weakened by the very thing that gave 
@@ -303,10 +310,10 @@ const setup = time => {
             appeared in 9 other movies. Which movie am I in?`
         )
         .add_clue(`My mentor was killed by my clone in this movie`, C)
-        .add_clue(`This actor has played this role for 17 years`, C)
+        .add_clue(`This actor has played this role for 17 years`, 2 * C)
         .set_answer(answers.shift())
-        .push_end(E);    
-    
+        .set_time(I);
+
     sman.add_stage()
         .set_question(
             `I am a cross country runner, a war veteran, 
@@ -316,9 +323,9 @@ const setup = time => {
         .add_clue(
             `The King of Pop has me to thank for his 
             signature dance move involving his legs`
-        , C)
+            , C)
         .set_answer(answers.shift())
-        .push_end(E);    
+        .set_time(I);
 
     sman.add_stage()
         .set_question(
@@ -331,9 +338,9 @@ const setup = time => {
             `One of the protagonists in this movie is 
             the brother of the Pulp Fiction character 
             Vincent Vega`
-        , C)
+            , C)
         .set_answer(answers.shift(), "Movie – Reservoir Dogs")
-        .push_end(E);
+        .set_time(I);
 
     sman.add_stage()
         .set_question(
@@ -355,11 +362,12 @@ const setup = time => {
                 picture-in-picture" 
                 allowfullscreen>
             </iframe>`
-        , C)
-        .add_clue(`Use the name of the corners to correlate it to a track`, C)
+            , C)
+        .add_clue(
+            `Use the name of the corners to correlate it to a track`
+        , 2 * C)
         .set_answer(answers.shift())
-        .push_end(E);
-
+        .set_time(I);
 
     sman.add_stage()
         .set_question(
@@ -371,7 +379,7 @@ const setup = time => {
             in a state close to Illinois.`
         , C)
         .set_answer(answers.shift())
-        .push_end(E);
+        .set_time(I);
 
     sman.add_stage()
         .set_question(
@@ -384,10 +392,9 @@ const setup = time => {
             `This movie’s director is also famous for his 
             work on one of the Twilight Saga films.`
         , C)
-        .add_clue(`The movie has a similar format to Minecraft Story Mode`, C)
+        .add_clue(`The movie has a similar format to Minecraft Story Mode`, 2 * C)
         .set_answer(answers.shift())
-        .push_end(E);
-
+        .set_time(I);
 
     sman.add_stage()
         .set_question(
@@ -397,33 +404,36 @@ const setup = time => {
             <img width="100%" src="gcircle.png" alt="god help you i cant describe this image">
             `
         )
-        .add_clue(`This movie stars Taron Egerton and Colin Firth`, C)
-        .set_answer(answers.shift(), 
+        .add_clue(
+            `This movie stars Taron Egerton and Colin Firth`
+        , 2 * C)
+        .set_answer(answers.shift(),
             `Kingsman: The Golden Circle 
             <br>Country Roads 
             <br>John Denver`
         )
-        .push_end(E);
+        .set_time(I);
 
-    sman.set_synchronized(false);
     sman.start_game();
 }
 
 const submit = () => {
-    sman.submit_answer(page.answer.value);
+    if(sman.submit_answer(page.answer.value)){
+        page.answer.value = "";
+    }
 }
 let update = () => {
     sman.update();
     window.requestAnimationFrame(update);
 }
 
-const pass_decode = code => {       
+const pass_decode = code => {
     const parity = x => {
         let [P, p] = [10, 0];
-    
-        while(x > P){
+
+        while (x > P) {
             p = (p + x) % P;
-            x = Math.floor(x/P);
+            x = Math.floor(x / P);
         }
         return p;
     };
@@ -432,11 +442,11 @@ const pass_decode = code => {
     const from_chr = c => c.charCodeAt(0) - CHAR_BEGIN;
 
     let n = 0;
-    for(let i = code.length - 1; i > 0; i--) {
+    for (let i = code.length - 1; i > 0; i--) {
         n = CHAR_WIDTH * n + from_chr(code[i]);
     }
 
-    if (parity(n) == from_chr(code[0])){
+    if (parity(n) == from_chr(code[0])) {
         return n;
     }
     return -1;
@@ -444,19 +454,33 @@ const pass_decode = code => {
 const start = () => {
     let code = $("input", $("#startmenu"))[0].value;
 
-    if(code.length > 0){
+    if (code.length > 0) {
         let t = pass_decode(code);
         console.log(code, t);
 
-        if(t !== -1){
+        if (t !== -1) {
             let T = t * 1000; 
-            console.log(`TIME ${T}`)
-
+            
             $("#startmenu").style.display = "none";
-            $("#content").style.display = "block";
+            $("#waitmenu").style.display = "block";
+            const wait = () => {
+                let t = T - Date.now();
 
-            setup(T);
-            update();    
+                if(t > 0){
+                    $("#waitmenu").innerHTML = `Hunt begins in ${format_time(t)}`
+                    console.log("Wait!");
+                    window.requestAnimationFrame(wait);
+                }
+                else {
+                    $("#waitmenu").style.display = "none";
+                    $("#content").style.display = "block";
+        
+                    setup(T);
+                    update();        
+                }
+            };
+            wait();
+
         }
         else {
             $("#startmenu-error").innerText = "Error: Enter valid code";
